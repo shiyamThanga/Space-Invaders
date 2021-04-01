aed11bb5df9624a969d509a803dcf8c3b0e2ab16
Kirushanth Thangavadivel <shiyamthanga@gmail.com>
2021-03-31 17:23:41 -0700

    Assets added

---
+++ Assets/Darwin/Scripts/Target.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+    public float health = 50f;
+
+    public AudioSource destroyedSound;
+
+
+    private void Start()
+    {
+        destroyedSound = GetComponent<AudioSource>();
+    }
+    public void takeDamage(float amount)
+    {
+        health -= amount;
+
+        if (health <= 0f)
+        {
+            die();
+        }
+    }
+
+    public void die()
+    {
+        destroyedSound.Play();
+        Destroy(gameObject);
+
+    }
+
+
+}
